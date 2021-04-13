21c7c711b00342ff6477f7d4cc01b7218e02abef
Melvin Macaranas <greatwinblowaway@gmail.com>
2021-04-13 18:53:24 +0700

    add printer 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { Container, Row, Col, Navbar, Card, Badge } from "react-bootstrap";
 
-function App() {
+import printImage from "./image/printer.png";
+
+const App = () => {
+  const [isAvailable, setIsAvailable] = useState(true);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar bg="dark" variant="dark" expand="lg">
+        <img
+          src={printImage}
+          alt="print"
+          style={{ height: "30px", marginRight: "16px" }}
+        />
+        <Navbar.Brand href="/">PaaS</Navbar.Brand>
+      </Navbar>
+      <div style={{ height: "100vh", width: "100vw", overflow: "hidden" }}>
+        <Container style={{ padding: "16px" }}>
+          <Row>
+            <Col>
+              <Card style={{ width: "18rem" }}>
+                <Card.Body>
+                  <Card.Title>Printer 1</Card.Title>
+                  <Badge variant={isAvailable ? "success" : "warning"}>
+                    {isAvailable ? "Available" : "Printing"}
+                  </Badge>
+                  {isAvailable ? null : (
+                    <Card.Text>Currently printing: {"Melvin"}</Card.Text>
+                  )}
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col>Print log here</Col>
+          </Row>
+        </Container>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
